98272535fa2597b23e7ecb56921f19532fc8c482
tawheeler <timwheeleronline@gmail.com>
2016-06-25 14:54:13 -0700

    more learning tests

--- test/test_learning.jl
+++ test/test_learning.jl
@@ -1,4 +1,38 @@
+type FakeScoringFunction <: ScoringFunction end
+
 let
+	function test_disc_bn(bn::DiscreteBayesNet)
+		#=
+		│ Row │ A │ count │
+		├─────┼───┼───────┤
+		│ 1   │ 1 │ 4     │
+		│ 2   │ 2 │ 4     │
+		│ 3   │ 3 │ 4     │
+		=#
+		df_count = count(bn, :A, data)
+		@test size(df_count) == (3,2)
+		@test select(df_count, Assignment(:A=>1))[:count] == [4]
+		@test select(df_count, Assignment(:A=>2))[:count] == [4]
+
+		#=
+		│ Row │ A │ B │ C │ count │
+		├─────┼───┼───┼───┼───────┤
+		│ 1   │ 1 │ 1 │ 1 │ 3     │
+		│ 2   │ 1 │ 2 │ 2 │ 1     │
+		│ 3   │ 2 │ 2 │ 1 │ 2     │
+		│ 4   │ 2 │ 2 │ 2 │ 1     │
+		│ 5   │ 2 │ 1 │ 1 │ 1     │
+		│ 6   │ 3 │ 1 │ 1 │ 3     │
+		│ 7   │ 3 │ 2 │ 2 │ 1     │
+		=#
+		df_count = count(bn, :C, data)
+		@test size(df_count) == (7,4)
+		@test select(df_count, Assignment(:A=>1, :B=>1))[:count] == [3]
+		@test select(df_count, Assignment(:A=>1, :B=>2))[:count] == [1]
+		@test select(df_count, Assignment(:A=>2, :B=>1))[:count] == [1]
+		@test select(df_count, Assignment(:A=>2, :B=>2))[:count] == [2,1]
+	end
+
 	data = DataFrame(A=[1,1,1,1,2,2,2,2,3,3,3,3],
                      B=[1,1,1,2,2,2,2,1,1,2,1,1],
                      C=[1,1,1,2,1,1,2,1,1,2,1,1])
@@ -8,37 +42,19 @@ let
 	add_edge!(dag, 1, 3)
 	add_edge!(dag, 2, 3)
 
+	bn = fit(BayesNet, data, dag, [DiscreteCPD, DiscreteCPD, DiscreteCPD])
+	bn = fit(BayesNet, data, (:A=>:B, :A=>:C, :B=>:C), [DiscreteCPD, DiscreteCPD, DiscreteCPD])
+	bn = fit(DiscreteBayesNet, data, :A=>:B)
+	bn = fit(BayesNet, data, :A=>:B, [DiscreteCPD, DiscreteCPD, DiscreteCPD])
+	bn = fit(BayesNet, data, :A=>:B, DiscreteCPD)
+
+	bn = fit(DiscreteBayesNet, data, (:A=>:B, :A=>:C, :B=>:C))
+	test_disc_bn(bn)
 	bn = fit(DiscreteBayesNet, data, dag)
+	test_disc_bn(bn)
 
-	#=
-	│ Row │ A │ count │
-	├─────┼───┼───────┤
-	│ 1   │ 1 │ 4     │
-	│ 2   │ 2 │ 4     │
-	│ 3   │ 3 │ 4     │
-	=#
-	df_count = count(bn, :A, data)
-	@test size(df_count) == (3,2)
-	@test select(df_count, Assignment(:A=>1))[:count] == [4]
-	@test select(df_count, Assignment(:A=>2))[:count] == [4]
+	@test_throws ErrorException score_component(FakeScoringFunction(), StaticCPD(:a, Bernoulli(0.5)), data)
 
-	#=
-	│ Row │ A │ B │ C │ count │
-	├─────┼───┼───┼───┼───────┤
-	│ 1   │ 1 │ 1 │ 1 │ 3     │
-	│ 2   │ 1 │ 2 │ 2 │ 1     │
-	│ 3   │ 2 │ 2 │ 1 │ 2     │
-	│ 4   │ 2 │ 2 │ 2 │ 1     │
-	│ 5   │ 2 │ 1 │ 1 │ 1     │
-	│ 6   │ 3 │ 1 │ 1 │ 3     │
-	│ 7   │ 3 │ 2 │ 2 │ 1     │
-	=#
-	df_count = count(bn, :C, data)
-	@test size(df_count) == (7,4)
-	@test select(df_count, Assignment(:A=>1, :B=>1))[:count] == [3]
-	@test select(df_count, Assignment(:A=>1, :B=>2))[:count] == [1]
-	@test select(df_count, Assignment(:A=>2, :B=>1))[:count] == [1]
-	@test select(df_count, Assignment(:A=>2, :B=>2))[:count] == [2,1]
 
 	# TODO: make this test more rigorous
 	@test length(count(bn, data)) == 3
