9f869121c327b98b445f5d64ac3094fb20004340
Anthony Perez <aperez8@corn17.stanford.edu>
2016-11-08 00:44:31 -0800

    Working on Gibbs sampling

--- src/sampling.jl
+++ src/sampling.jl
@@ -108,6 +108,86 @@ function rand_table_weighted(bn::BayesNet; nsamples::Integer=10, consistent_with
     convert(DataFrame, t)
 end
 
+function sample_posterior_discrete(bn::BayesNet, varname::Symbol, a::Assignment)
+   num_categories = # TODO
+
+   markov_blanket_cdps = [get(bn, child_name) for child_name in children(bn, varname)]
+   push!(markov_blanket_cdps, get(bn, varname))
+
+   posterior_distribution = zeros(num_categories)
+   for index in 1:num_categories
+       a[varname] = index
+       # Sum logs for numerical stability
+       posterior_distribution[index] = exp(sum([logpdf(cdp, a) for cdp in markov_blanket_cdps]))
+   end
+   posterior_distribution = posterior_distribution / sum(posterior_distribution)
+
+   # Adapted from Distributions.jl, credit to its authors
+   p = posterior_distribution
+   n = length(p)
+   i = 1
+   c = p[1]
+   u = rand()
+   while c < u && i < n
+       c += p[i += 1]
+   end
+   return i
+
+end
+
+"""
+Temporarily modifies a, but restores it after computations
+TODO check if arguments are copied or passed by reference
+"""
+function sample_posterior(bn::BayesNet, varname::Symbol, a::Assignment)
+    original_value = a[varname]
+    new_value = # TODO
+    a[varname] = original_value
+    return new_value
+end
+
+function gibbs_sample_main_loop(bn::BayesNet, nsamples::Integer, sample_skip::Integer, 
+start_sample::Assignment, consistent_with::Assignment, variable_order::Nullable{Vector{Symbol}},
+time_limit::Nullable{Integer})
+
+    t = Dict{Symbol, Vector{Any}}()
+    for name in names(bn)
+        t[name] = Any[]
+    end
+
+    w = ones(Float64, nsamples) # TODO is this needed
+    a = start_sample
+    if isnull(variable_order)
+         v_order = names(bn)
+    else
+         v_order = get(variable_order)
+    end
+
+    for sample_iter in 1:nsamples
+        if isnull(variable_order)
+             v_order = shuffle!(v_order)
+        end
+
+        for varname in v_order
+
+            if haskey(consistent_with, varname)
+                # TODO what to do here?
+                cpd = get(bn, varname)
+                a[varname] = consistent_with[varname]
+                w[i] *= pdf(cpd, a)
+            else
+                a[varname] = sample_posterior(bn, varname, a)
+            end
+            push!(t[varname], a[varname])
+
+        end
+
+    end
+
+    t[:p] = w / sum(w)
+    convert(DataFrame, t)
+end
+
 """
 TODO description
 
@@ -117,11 +197,34 @@ sample_skip: every (sample_skip + 1)th sample will be used, the other sample_ski
 variable_order: variable_order determines the order of variables changed when generating a new sample, if variable_order is None, then a random variable order will be used for each sample.
 """
 function gibbs_sample(bn::BayesNet, nsamples::Integer, burn_in::Integer; sample_skip::Integer=99,
-consistent_with::Assignment=Assignment(), variable_order::Nullable{Vector{Symbol}}=Nullable{Vector{Symbol}}, 
-time_limit::Nullable{Integer}=Nullable{Integer}())
-"""
-TODO algorithm
-TODO unit test under test/
-TODO come up with an automatic method for setting the burn_in period, look at literatures.  Once this is implemented, move burn_in to the default parameters
-"""
+consistent_with::Assignment=Assignment(), variable_order::Nullable{Vector{Symbol}}=Nullable{Vector{Symbol}}(), 
+time_limit::Nullable{Integer}=Nullable{Integer}(), error_if_time_out::Bool=true, 
+inital_sample::Nullable{Assignment}=Nullable{Assignment}())
+    """
+    TODO algorithm
+    TODO unit test under test/
+    TODO come up with an automatic method for setting the burn_in period, look at literatures.  
+              Once this is implemented, move burn_in to the default parameters
+    """
+    # TODO check parameters for correctness, variable_order should be null or provide an order for all variables
+    # check that initial_sample is either null or a full assignment that is consistent with the variable consistent_with
+   
+    # Burn in 
+    burn_in_initial_sample = # TODO use rand_table_weighted, should be consistent with the varibale consistent_with
+    burn_in_samples, burn_in_time = gibbs_sample_main_loop(bn, burn_in, 1, burn_in_initial_sample, 
+                                         consistent_with, variable_order, time_limit)
+    if error_if_time_out && ~isnull(time_limit)
+        time_limit - burn_in_time > 0 || error("Time expired during Gibbs sampling")
+    end
+   
+    # Real samples
+    main_samples_initial_sample = # TODO 
+    samples, samples_time = gibbs_sample_main_loop(bn, nsamples, sample_skip, 
+                               main_samples_initial_sample, consistent_with, variable_order, remaining_time)
+    combined_time = burn_in_time + samples_time
+    if error_if_time_out && ~isnull(time_limit)
+        combined_time < get(time_limit) || error("Time expired during Gibbs sampling")
+    end
+
+    return samples
 end
