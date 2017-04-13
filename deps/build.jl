923d8b7a9baa5a10344cdb7f2982e49befe43802
Tim Wheeler <tawheeler@users.noreply.github.com>
2017-04-13 12:06:55 -0700

    Update build.jl

--- deps/build.jl
+++ deps/build.jl
@@ -2,5 +2,5 @@
 try
     Pkg.clone("https://github.com/sisl/ProbabilisticGraphicalModels.jl.git")
 catch e
-    println("Exception when cloning $(url): $(e)")
+    println("Exception when cloning ProbabilisticGraphicalModels.jl: $(e)")
 end
