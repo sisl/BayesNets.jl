091c74fad40b23f1044448bdcc5615c0cf4bb2d2
Tim Wheeler <tawheeler@users.noreply.github.com>
2017-06-10 15:33:40 -0700

    Create build.jl
    no need to install PGM

--- deps/build.jl
+++ deps/build.jl
@@ -1,6 +1,7 @@
-
+#=
 try
     Pkg.clone("https://github.com/sisl/ProbabilisticGraphicalModels.jl.git")
 catch e
     println("Exception when cloning ProbabilisticGraphicalModels.jl: $(e)")
 end
+=#
