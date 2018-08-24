29ccf9cbfc7cfd93160c7160ec124a16167f9b16
rejuvyesh <mail@rejuvyesh.com>
2018-08-24 15:48:46 -0700

    some more select to partialsort

--- test/test_learning.jl
+++ test/test_learning.jl
@@ -13,8 +13,8 @@ let
 		=#
 		df_count = count(bn, :A, data) |> Table
 		@test size(df_count) == (3,2)
-		@test select(df_count, Assignment(:A=>1))[:count] == [4]
-		@test select(df_count, Assignment(:A=>2))[:count] == [4]
+		@test partialsort(df_count, Assignment(:A=>1))[:count] == [4]
+		@test partialsort(df_count, Assignment(:A=>2))[:count] == [4]
 
 		#=
 		│ Row │ A │ B │ C │ count │
@@ -29,10 +29,10 @@ let
 		=#
 		df_count = count(bn, :C, data) |> Table
 		@test size(df_count) == (7,4)
-		@test select(df_count, Assignment(:A=>1, :B=>1))[:count] == [3]
-		@test select(df_count, Assignment(:A=>1, :B=>2))[:count] == [1]
-		@test select(df_count, Assignment(:A=>2, :B=>1))[:count] == [1]
-		@test select(df_count, Assignment(:A=>2, :B=>2))[:count] == [2,1]
+		@test partialsort(df_count, Assignment(:A=>1, :B=>1))[:count] == [3]
+		@test partialsort(df_count, Assignment(:A=>1, :B=>2))[:count] == [1]
+		@test partialsort(df_count, Assignment(:A=>2, :B=>1))[:count] == [1]
+		@test partialsort(df_count, Assignment(:A=>2, :B=>2))[:count] == [2,1]
 	end
 
 	data = DataFrame(A=[1,1,1,1,2,2,2,2,3,3,3,3],
--- test/test_tables.jl
+++ test/test_tables.jl
@@ -13,23 +13,23 @@ let
     # factor multiplication
     f12 = f1 * f2
     @test size(f12) == (4,3)
-    @test elementwise_isapprox(select(f12, Assignment(:A=>false, :B=>false))[:p], [0.75*0.9])
-    @test elementwise_isapprox(select(f12, Assignment(:A=>true,  :B=>false))[:p], [0.60*0.1])
-    @test elementwise_isapprox(select(f12, Assignment(:A=>false, :B=>true))[:p], [0.25*0.9])
-    @test elementwise_isapprox(select(f12, Assignment(:A=>true,  :B=>true))[:p], [0.40*0.1])
+    @test elementwise_isapprox(partialsort(f12, Assignment(:A=>false, :B=>false))[:p], [0.75*0.9])
+    @test elementwise_isapprox(partialsort(f12, Assignment(:A=>true,  :B=>false))[:p], [0.60*0.1])
+    @test elementwise_isapprox(partialsort(f12, Assignment(:A=>false, :B=>true))[:p], [0.25*0.9])
+    @test elementwise_isapprox(partialsort(f12, Assignment(:A=>true,  :B=>true))[:p], [0.40*0.1])
 end
 
 let
     # factor marginalization
     f1_sans_B = sumout(f1, :B)
     @test size(f1_sans_B) == (2,2)
-    @test elementwise_isapprox(select(f1_sans_B, Assignment(:A=>false))[:p], [0.75 + 0.25])
-    @test elementwise_isapprox(select(f1_sans_B, Assignment(:A=>true))[:p], [0.60 + 0.40])
+    @test elementwise_isapprox(partialsort(f1_sans_B, Assignment(:A=>false))[:p], [0.75 + 0.25])
+    @test elementwise_isapprox(partialsort(f1_sans_B, Assignment(:A=>true))[:p], [0.60 + 0.40])
 
     f1_sans_A = sumout(f1, :A)
     @test size(f1_sans_A) == (2,2)
-    @test elementwise_isapprox(select(f1_sans_A, Assignment(:B=>false))[:p], [0.75 + 0.60])
-    @test elementwise_isapprox(select(f1_sans_A, Assignment(:B=>true))[:p], [0.25 + 0.40])
+    @test elementwise_isapprox(partialsort(f1_sans_A, Assignment(:B=>false))[:p], [0.75 + 0.60])
+    @test elementwise_isapprox(partialsort(f1_sans_A, Assignment(:B=>true))[:p], [0.25 + 0.40])
 end
 
 let
