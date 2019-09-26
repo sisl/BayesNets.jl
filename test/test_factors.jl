2448a3b187381914e220b1ef4853e292f36c606e
Robert Moss <rmoss92@gmail.com>
2019-09-25 19:05:55 -0700

    Fixed runtests: qualified LinearAlgrebra.normalize in test_factors.jl

--- test/test_factors.jl
+++ test/test_factors.jl
@@ -66,14 +66,14 @@ end
 #                   normalize
 let
 ϕ = Factor([:a, :b], Float64[1 2; 3 4])
-ϕ2 = normalize(ϕ, p=1)
+ϕ2 = LinearAlgebra.normalize(ϕ, p=1)
 
 @test elementwise_isapprox(ϕ2.potential, [0.1 0.2; 0.3 0.4])
 @test elementwise_isapprox(ϕ.potential, Float64[1 2; 3 4])
 
-@test_throws ArgumentError normalize(ϕ, :waldo)
+@test_throws ArgumentError LinearAlgebra.normalize(ϕ, :waldo)
 
-normalize!(ϕ, p=2)
+LinearAlgebra.normalize!(ϕ, p=2)
 
 @test elementwise_isapprox(ϕ.potential, [1/30 2/30; 1/10 4/30])
 end
